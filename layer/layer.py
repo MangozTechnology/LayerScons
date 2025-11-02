759d77152ba112a8e6a5d62303f7eb07d695d409
MangozTechnology <mangoztechnology@gmail.com>
2025-11-02 15:52:16 -0600

    Create layer.py

---
+++ layer/layer.py
@@ -0,0 +1,16 @@
+"""
+ layer.py 
+ praise the Lord, for my code
+"""
+
+class Layer:
+  # layer id info
+  layerId = 0
+  undoId = 0
+  redoId = 0
+
+  def CreateNewLayerA(Layer, points):
+    any()
+    
+  def CreateNewLayerW(Layer, points, label):
+    any()
